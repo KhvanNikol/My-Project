66d0296e62c1e5024976963d81d4718b34bf579a
KhvanNikol <109013298+KhvanNikol@users.noreply.github.com>
2022-08-25 15:29:11 +0900

    added lon&lat

--- src/java2.js
+++ src/java2.js
@@ -46,7 +46,7 @@ function handleSubmit(event) {
 }
 function searchLocation(position) {
   let apiKey = "8bd19818f1a9f1fdc34f62c973ba9f77";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?&lat${position.coords.latitude}&${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?&lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayWeather);
 }
 
